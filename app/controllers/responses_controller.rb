cd94735b875ecf630385822c2911669bcd7e7075
frisk <friskslalom6@Randalls-MacBook-Pro-2.local>
2013-09-24 06:36:05 -0700

    Finished first version of Viewing Survey responses

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,4 +1,5 @@
 class ResponsesController < ApplicationController
+  before_filter :authorize
   def new
   	@bootcamp = Bootcamp.find(params[:bootcamp_id])
   	@survey = Survey.find(params[:survey_id])
@@ -12,4 +13,14 @@ class ResponsesController < ApplicationController
   		redirect_to new_survey_response_answer_path(@response.survey_id, @response.id)
   	end
   end
+
+  def index
+    @bootcamp = Bootcamp.find(params[:bootcamp_id])
+    @survey = Survey.find(params[:survey_id])
+    @questions = @survey.questions.order("rated")
+    @columns = @survey.questions.where(rated: 0).count
+    @responses = @bootcamp.responses.find_all_by_survey_id(@survey.id)
+    @rcount = @responses.count
+  end
+
 end
